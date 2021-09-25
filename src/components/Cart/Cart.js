392cc0afe4ba638e5fc93efa2c2f4847515a82ef
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-09-26 04:05:33 +0600

    added dynamic cart item count

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+
 const Cart = (props) => {
     console.log(props.cart);
+    const { name, fee } = props.cart || {};
+
+
     return (
         <div>
-
+            <h3><i className="fas fa-user-check"></i>Experts Added: {props.cart.length}</h3>
         </div>
     );
 };
