e32e340d014cea57937ea1214cd9161fa1ea14b7
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-09-26 01:54:23 +0600

    added header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="p-2 text-success">
+                <h2>Hire a Team of Expert Programmers</h2>
+                <p>P Network is an exclusive network of the top programmers in the world. Top companies can hire these top programmers for their most complex projects</p>
+                <h2> Total Budget: 150 Million </h2>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
