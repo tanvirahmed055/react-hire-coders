7f6df3d71eedb34c602615f5930f51b7cc1c1423
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-09-26 17:51:32 +0600

    made minor changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <div className="header mb-5">
             <div className="p-2">
                 <h2>Hire a Team of Expert Programmers</h2>
-                <p><span className="fw-bolder">P Experts Network</span> is an exclusive network of the top programmers in the world. Top companies can hire these top programmers for their most complex projects</p>
+                <p><span className="fw-bolder">HireCoders</span> is a freelance platform of the top programmers in the world. Top companies can hire these top programmers for their most complex projects</p>
                 <h2> Total Budget: <span className="fw-bolder">150 Million</span> </h2>
             </div>
         </div>
