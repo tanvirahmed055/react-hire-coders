f6aec3e7bdb40649c8122f2273cd955df0edf6ee
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-09-26 19:14:50 +0600

    made changes in card icon styles

--- src/components/Expert/Expert.js
+++ src/components/Expert/Expert.js
@@ -13,16 +13,16 @@ const Expert = (props) => {
                     <img src={img} className="card-img-top product-image" alt="..." />
                 </div>
                 <div className="card-body">
-                    <h6 className="card-title fw-bolder text-center">Name:<span className="fw-normal"> {name}</span></h6>
-                    <p className="card-text text-dark text-center fw-bolder">Skill: <span className="fw-normal">{skill} </span></p>
-                    <p className="card-text text-dark text-center fw-bolder">Experience: <span className="fw-normal">{experience}  years</span></p>
-                    <p className="card-text text-dark text-center fw-bolder">From: <span className="fw-normal">{location}</span></p>
-                    <p className="fw-bold text-center fw-bolder">Fee Per Month:  <span className="fw-normal">${fee}</span></p>
+                    <h6 className="card-title fw-bolder text-center text-success">Name:<span className="fw-normal text-dark"> {name}</span></h6>
+                    <p className="card-text text-center fw-bolder text-success">Skill: <span className="fw-normal text-dark">{skill} </span></p>
+                    <p className="card-text text-center fw-bolder text-success">Experience: <span className="fw-normal text-dark">{experience}  years</span></p>
+                    <p className="card-text text-center fw-bolder text-success">From: <span className="fw-normal text-dark">{location}</span></p>
+                    <p className="fw-bold text-center fw-bolder text-success">Fee Per Month:  <span className="fw-normal text-dark">${fee}</span></p>
                 </div>
                 <div className="d-grid gap-2 col-9 mx-auto">
                     <button className="btn btn-success" type="button" onClick={() => props.handleAddToCart(props.expert)}><i className="fas fa-shopping-cart"></i> Add To Cart</button>
                 </div>
-                <div className=" mx-auto mt-3">
+                <div className=" mx-auto mt-3 text-secondary">
                     <i className="fab fa-facebook-square fa-2x icon"></i>
                     <i className="fab fa-linkedin fa-2x icon"></i>
                     <i className="fab fa-twitter-square fa-2x icon"></i>
